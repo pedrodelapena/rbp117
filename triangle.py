22ea120f3ef839078a188e52fcd5cb338b4a0c16
pedrodelapena <pedrodelapena@uol.com.br>
2017-03-22 21:53:46 -0300

    Add files via upload

--- triangle.py
+++ triangle.py
@@ -1,12 +1,12 @@
 # -*- coding:utf-8 -*-
 
-class Triangle()
+class Triangle:
 
     def __init__(self, coord):
         self.red= coord[0]
         self.green= coord[1]
         self.blue= coord[2]
-        self.center=( (coord[0][0]+coord[2][0])/2, (coord[0][1]+coord[2][1])/2 )
+        self.center=( (min(coord[0][0], coord[2][0], coord[1][0])+max(coord[0][0], coord[2][0], coord[1][0]))/2, (min(coord[0][1], coord[2][1], coord[1][1])+max(coord[0][1], coord[2][1], coord[1][1]))/2 )
         #self.rotation
         #self.angle
         #self.distance
